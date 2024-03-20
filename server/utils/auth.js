83a4cca7ebcf04d9afc0e02373291d483f492494
Kurstie DeHaven-Vargas <kurstiedehaven@gmail.com>
2024-03-19 21:28:50 -0400

    heroku redeployment

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -31,10 +31,10 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // return req;
+    return req;
 
     // send to next endpoint
 
